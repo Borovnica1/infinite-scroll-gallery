859e519e69e599409c48def202f5978cc54ba1a1
Borovnica1 <petkovic.czv1@gmail.com>
2021-10-11 22:06:23 +0200

    add api

--- script.js
+++ script.js
@@ -18,8 +18,28 @@ function switchViewMode() {
   el.addEventListener('click', switchViewMode);
 });
 
-function removeLoader() {
+function startEndLoader() {
   loader.classList.toggle('loader-overlay--active');
 };
 
-setTimeout(removeLoader, 2000);
+async function getPhotos() {
+  startEndLoader();
+  const response = await fetch('https://api.unsplash.com/photos/random?count=6&client_id=LVTQ3W33_bo9qp9FRXvnNs3DZZj8Bo9ucA84Aww9jU4');
+  const photosArray = await response.json();
+
+  startEndLoader();
+  displayPhotos(photosArray);
+};
+
+function displayPhotos(photosArray) {
+  console.log('LOL', photosArray);
+  photosArray.forEach(photo => {
+    const photoHtml = `
+    <a href="${photo.links.html}" class="images__image">
+      <img src="${photo.urls.regular}" alt="${photo.alt_description ? photo.alt_description : 'Regular image'}" title="${photo.alt_description ? photo.alt_description : 'Regular image'}">
+    </a>`;
+    imagesView.insertAdjacentHTML('beforeend', photoHtml);
+  });
+};
+
+getPhotos();
